1bdfe67bae3869977eb3caecc5bc0280a8af3417
Jakub Jalowiec <01103622@pw.edu.pl>
2019-11-24 00:19:04 +0100

    Deleting HSTS

--- app/Startup.cs
+++ app/Startup.cs
@@ -59,7 +59,6 @@ namespace app
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseStaticFiles();
