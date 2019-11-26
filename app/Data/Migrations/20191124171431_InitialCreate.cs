f8ca6adf4b86dd2a942065bd80f64bc5130556a4
Jakub Jalowiec <01103622@pw.edu.pl>
2019-11-26 21:59:21 +0100

    #1 testing migrations

--- app/Data/Migrations/20191124171431_InitialCreate.cs
+++ app/Data/Migrations/20191124171431_InitialCreate.cs
@@ -6,12 +6,22 @@ namespace app.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+                name: "TestNowy",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestNowy", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "TestNowy");
         }
     }
 }
