1c2697ae119673e592546d9e6ea43a0db0ae0f85
KMD\Z6KLM <klm@kmd.dk>
2019-12-19 18:54:28 +0100

    hope the list of events will be seen :)

--- app/ClientApp/src/app/events/events-list/events-list.component.ts
+++ app/ClientApp/src/app/events/events-list/events-list.component.ts
@@ -13,10 +13,23 @@ export class EventsListComponent implements OnInit {
   eventsList = Array<MyEvent>();
   constructor(private eventService: EventService) { }
 
+  // ngOnInit() {
+  //   this.eventService.getEvents().subscribe({
+  //     next: (events) => this.eventsList = events,
+  //     error: () => alert('Nie udało się pobrać wydarzeń')
+  //   });
+  // }
+
   ngOnInit() {
-    this.eventService.getEvents().subscribe({
-      next: (events) => this.eventsList = events,
-      error: () => alert('Nie udało się pobrać wydarzeń')
+    this.eventService.getEvents().subscribe(events => {
+      for (const e of (events as Array<MyEvent>)) {
+        this.eventsList.push({
+          name: e.name,
+          place: e.place,
+          date: e.date,
+          description: e.description
+        });
+      }
     });
   }
 
